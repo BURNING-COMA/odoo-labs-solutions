2af0c4758fe7e6fa201ff6b4e18b3a31f6d13dc0
BURING-COMA <xmanhycal@gmail.com>
2022-02-14 15:18:30 +0200

    lab 3: add email field with format check and uniqueness check

--- hms/models/hms_patient.py
+++ hms/models/hms_patient.py
@@ -1,7 +1,7 @@
+import re
 
-from codeop import CommandCompiler
-from dataclasses import field
-from odoo import models, fields, api
+from odoo import models, fields, api 
+from odoo.exceptions import ValidationError
 
 class HmsPatient( models.Model ):
     _name = 'hms.patient'
@@ -30,9 +30,31 @@ class HmsPatient( models.Model ):
     address = fields.Text() 
     department_id = fields.Many2one(comodel_name='hms.department')
     doctors_ids = fields.Many2many(comodel_name='hms.doctor')
+    # TODO complete last step in lab 2 related to creating a log whenever state changes
     log_id = fields.Many2one(comodel_name='hms.patient.log')
 
     department_capacity = fields.Integer(related='department_id.capacity')
+
+    # DONE add API constraint is_valide,
+    # DONE sql constrain UNIQUE 
+    email = fields.Char()
+
+
+    _sql_constraints = [
+        ("email_uniqueness", "UNIQUE(email)", "Email is already registered.")
+    ]
+
+    @api.constrains('email')
+    def _check_email(self):
+        for record in self: 
+            # DONE use python regix to validate email ..
+            # https://stackoverflow.com/questions/8022530/how-to-check-for-valid-email-address
+            # https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
+            # just basic email check, no need for compelx check since further verifications will be done anyway 
+            if not re.fullmatch(r"[^@]+@[^@]+\.[^@]+", record.email): 
+                raise ValidationError(f"'{record.email}' is not a valid email.")
+
+
 
 
     @api.onchange('age')
